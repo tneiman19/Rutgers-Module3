69cd90c6971d67ed313d78e4ec50799f3b1c9451
Tzvi Neiman <tneiman19@gmail.com>
2023-01-12 01:33:22 -0500

    Added JS to run through the promts to find out the users password criteria.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,8 +7,58 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var generatePassword = function () {
+  var passwordCriteria = {
+    passwordLength: 0,
+    includeLowercase: false,
+    includeUppercase: false,
+    includeNumeric: false,
+    includeSpecial: false,
+  };
+
+  while (
+    passwordCriteria.passwordLength < 8 ||
+    passwordCriteria.passwordLength > 128
+    //add logic to make sure a number was entered
+  ) {
+    passwordCriteria.passwordLength = Math.floor(
+      parseFloat(prompt("Enter password length\n(Between 8-128 charecters)"))
+    );
+  }
+  if (!passwordCriteria.passwordLength) {
+    return "error: Password generator canceled by user";
+  } else {
+    // Ask user if we should include lowercase letters
+    passwordCriteria.includeLowercase = confirm(
+      "Do you want to include lowercase letters in your password?"
+    );
+    // Ask user if we should include uppercase letters
+    passwordCriteria.includeUppercase = confirm(
+      "Do you want to include uppercase letters in your password?"
+    );
+    // Ask user if we should include numbers
+    passwordCriteria.includeNumeric = confirm(
+      "Do you want to include numbers in your password?"
+    );
+    // Ask user if we should include special characters
+    passwordCriteria.includeSpecial = confirm(
+      "Do you want to include lowercase letter in your password?"
+    );
+  }
+
+  if (
+    !passwordCriteria.includeLowercase &&
+    !passwordCriteria.includeUppercase &&
+    !passwordCriteria.includeNumeric &&
+    !passwordCriteria.includeSpecial
+  ) {
+    alert("error: You must select at at lease 1 charecter type!");
+    generatePassword();
+  } else alert("Working On It!");
+  return "Working on it!";
+};
