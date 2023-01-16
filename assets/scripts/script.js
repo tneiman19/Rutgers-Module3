e9354a61d046975da1a28e5bbe2f6ba2e8764aef
Tzvi Neiman <tneiman19@gmail.com>
2023-01-15 19:24:16 -0500

    Added function to reset the criteria, validate user input, ans generate a password

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,12 +12,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowercaseFields = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseFields = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numericFields = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialFields = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", ":", ";", "'", '"', "<", ">", ".", "?", "/"];
-var selectedFields = [];
-
 var passwordCriteria = {
   passwordLength: 0,
   includeLowercase: false,
@@ -26,48 +20,90 @@ var passwordCriteria = {
   includeSpecial: false,
 };
 
+var lowercaseFields = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseFields = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericFields = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialFields = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", ":", ";", "'", '"', "<", ">", ".", "?", "/", "\\"];
+var selectedFields = [];
+
 var generatePassword = function () {
+  resetCriteria();
   while (
     passwordCriteria.passwordLength < 8 ||
     passwordCriteria.passwordLength > 128
     //add logic to make sure a number was entered
   ) {
-    passwordCriteria.passwordLength = Math.floor(parseFloat(prompt("Enter password length\n(Between 8-128 charecters)")));
+    passwordCriteria.passwordLength = parseInt(
+      prompt("Enter password length\n(Between 8-128 charecters)")
+    );
   }
   if (!passwordCriteria.passwordLength) {
     return "error: Password generator canceled by user";
   } else {
     // Ask user if we should include lowercase letters
-    passwordCriteria.includeLowercase = confirm("Do you want to include lowercase letters in your password?");
+    passwordCriteria.includeLowercase = confirm(
+      "Do you want to include lowercase letters in your password?"
+    );
 
     if (passwordCriteria.includeLowercase) {
       selectedFields = [...selectedFields, ...lowercaseFields];
     }
 
     // Ask user if we should include uppercase letters
-    passwordCriteria.includeUppercase = confirm("Do you want to include uppercase letters in your password?");
+    passwordCriteria.includeUppercase = confirm(
+      "Do you want to include uppercase letters in your password?"
+    );
 
     if (passwordCriteria.includeUppercase) {
       selectedFields = [...selectedFields, ...uppercaseFields];
     }
 
     // Ask user if we should include numbers
-    passwordCriteria.includeNumeric = confirm("Do you want to include numbers in your password?");
+    passwordCriteria.includeNumeric = confirm(
+      "Do you want to include numbers in your password?"
+    );
 
     if (passwordCriteria.includeNumeric) {
       selectedFields = [...selectedFields, ...numericFields];
     }
 
     // Ask user if we should include special characters
-    passwordCriteria.includeSpecial = confirm("Do you want to include special characters in your password?");
+    passwordCriteria.includeSpecial = confirm(
+      "Do you want to include special characters in your password?"
+    );
 
     if (passwordCriteria.includeSpecial) {
       selectedFields = [...selectedFields, ...specialFields];
     }
+    return validation();
   }
-
-  if (!passwordCriteria.includeLowercase && !passwordCriteria.includeUppercase && !passwordCriteria.includeNumeric && !passwordCriteria.includeSpecial) {
-    return "error: You must select at at lease 1 charecter type!";
-  }
-  return "Working on it!";
 };
+
+function resetCriteria() {
+  passwordCriteria.passwordLength = 0;
+  passwordCriteria.includeLowercase = false;
+  passwordCriteria.includeUppercase = false;
+  passwordCriteria.includeNumeric = false;
+  passwordCriteria.includeSpecial = false;
+  selectedFields = [];
+}
+
+function validation() {
+  if (
+    !passwordCriteria.includeLowercase &&
+    !passwordCriteria.includeUppercase &&
+    !passwordCriteria.includeNumeric &&
+    !passwordCriteria.includeSpecial
+  ) {
+    return "error: You must select at at lease 1 charecter type!";
+  } else return creatingPassword();
+}
+
+function creatingPassword() {
+  var tempPassword = "";
+  for (let i = 0; i < passwordCriteria.passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * selectedFields.length);
+    tempPassword = tempPassword.concat(selectedFields[randomIndex]);
+  }
+  return tempPassword;
+}
